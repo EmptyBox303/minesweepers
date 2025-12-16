494757ed2b1cd7b5b41cc56683526dcdba00def9
EmptyBox303 <JasonLu2027@u.northwestern.edu>
2025-12-16 17:08:47 -0600

    solve now reads in dimensional params

--- solve.cpp
+++ solve.cpp
@@ -1,6 +1,7 @@
 using namespace std;
 
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <cstring>
 #include <cstdlib>
@@ -9,10 +10,49 @@ using namespace std;
 #include <fstream>
 #include <utility>
 #include <queue>
+#include <vector>
+#include "head.hpp"
 
+int width;
+int height;
+int mineCount;
+int spawnX;
+int spawnY;
+
+char field[MAX_WIDTH][MAX_HEIGHT];
 
 int main(){
+    //open mineField for reading
     ifstream mineField("outputs/minefield.txt");
+
+    //prepping buffer
+    string line;
+
+    //kill program if minefield not found
+    if (!mineField.is_open()){
+        cout << "couldn't open minefield; exiting\n";
+        return 1;
+    }
+
+    //read out parameters
+    getline(mineField,line);
+    stringstream params(line);
+    string token;
+    vector<string> tokens;
+    while(getline(params,token,' ')){
+        tokens.push_back(token);
+    }
+
+    //convert parameters to numerics
+    width = stoi(tokens[0]);
+    height = stoi(tokens[1]);
+    mineCount = stoi(tokens[2]);
+    spawnX = stoi(tokens[3]);
+    spawnY = stoi(tokens[4]);
+
+
+    
+    
     return 0;
 }
 
@@ -24,3 +64,5 @@ int main(){
 //what tiles are of interest?
 //those on the boundary?
 //we start out with the spawn point tile as tile of interest
+
+//
