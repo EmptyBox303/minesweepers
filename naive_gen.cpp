7e367c3b6f454fe5f4596e408dc915986cb3c30d
EmptyBox303 <JasonLu2027@u.northwestern.edu>
2025-12-15 20:59:18 -0600

    setup

--- naive_gen.cpp
+++ naive_gen.cpp
@@ -1,1 +1,24 @@
+using namespace std;
+#include <stdlib>
 
+//function of the program:
+//naive-gen width height mcount
+#define MAX_WIDTH 50
+#define MAX_HEIGHT 50
+#define MAX_DENSITY 0.25
+
+int main(int argc, char* argv[]){
+
+    //invalidate improper input
+    if (argc != 4){
+        cout << "usage: naive-gen width height mcount\n";
+        return 1; 
+    }
+    
+    int width = stoi(argv[2]);
+    int height = stoi(argv[3]);
+    int mineCount = stoi(argv[4]);
+    
+    
+    return 0;
+}
