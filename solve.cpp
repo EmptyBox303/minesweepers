de8aa960c6e55bbede829929b6ec21f02eb50486
EmptyBox303 <JasonLu2027@u.northwestern.edu>
2025-12-16 13:09:09 -0600

    created solve file

---
+++ solve.cpp
@@ -0,0 +1,26 @@
+using namespace std;
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <fstream>
+#include <utility>
+#include <queue>
+
+
+int main(){
+    
+    return 0;
+}
+
+
+//read in info
+//start from starting tile
+
+//we need to keep track of a set of "boundary" tiles that are of interest
+//what tiles are of interest?
+//those on the boundary?
+//we start out with the spawn point tile as tile of interest
